cb2738c87fe3ebfdd9b9fc501c332cc822c9c49b
miguelraatz <miguelraatz_@hotmail.com>
2023-09-23 20:55:00 -0300

    feat: implemented logo image

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { NavLink, useHistory } from 'react-router-dom';
-import logo from '../images/logo.png'
+import logo from '../images/logo.png';
 import '../styles/Login.css';
 
 function Login() {
