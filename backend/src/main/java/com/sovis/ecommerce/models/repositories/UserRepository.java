56749ad46bebff0dc1ee4649f6daf70d648550d3
miguelraatz <miguelraatz_@hotmail.com>
2023-09-23 20:59:55 -0300

    feat: implemented entitie user and repository

---
+++ backend/src/main/java/com/sovis/ecommerce/models/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.sovis.ecommerce.models.repositories;
+
+import com.sovis.ecommerce.models.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository UserRepository.
+ */
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
